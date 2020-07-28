2e000d9b3bfac7f311c2f06de4b26c23cac7acdd
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2020-07-29 00:54:51 +0200

    Move hoodfile overview to hoodfiles.php script
    This moves the hood file overview into a separate file hoodfiles.php,
    so that we can drop the GET parameter entirely.
    
    hoods.php?hoodfile -> hoodfiles.php
    
    This allows to optimize code to the specific needs of each page.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

---
+++ hoodfiles.php
@@ -0,0 +1,36 @@
+<?php
+
+require "function.php";
+
+$polydata = getPolyhoodsByHood(); // read polygon data for later use
+
+try {
+	$q = 'SELECT ID, name, lat, lon, ESSID_AP FROM hoods;';
+	$rs = db::getInstance()->prepare($q);
+	$rs->execute();
+} catch (PDOException $e) {
+	exit(showError(500, $e));
+}
+
+while ( $result = $rs->fetch ( PDO::FETCH_ASSOC ) ) {
+
+	$ispoly = false;
+	if(isset($polydata[$result['ID']])) {
+		$polygons = array_values($polydata[$result['ID']]); // we don't need the polyids here
+
+		$sumlat = 0;
+		$sumlon = 0;
+		// calculate average coordinates of first polygon; this may give wrong coordinates, but this is relatively unlikely
+		foreach($polygons[0] as $poly) {
+			$sumlat += $poly['lat'];
+			$sumlon += $poly['lon'];
+		}
+		$result['lat'] = $sumlat / count($polygons[0]);
+		$result['lon'] = $sumlon / count($polygons[0]);
+		$ispoly = true;
+	}
+
+	echo 'ID: '.$result['ID'].' ; Name: '.$result['name'].' ; lat: '.$result['lat'].' ; lon: '.$result['lon'].' ; type: '.($ispoly ? 'poly' : 'classic').' ; <a href="http://keyserver.freifunk-franken.de/v2/?lat='.$result['lat'].'&long='.$result['lon'].'">zum Hoodfile</a><br>';
+}
+
+?>
